026298005d3e69fe3c135a5a2a73144e1196cb71
Armin Sadr <arminsadr164@hotmail.com>
2023-03-29 01:38:00 +0200

    Remove the createHeader function and add it to its own module

---
+++ src/createHeader.js
@@ -0,0 +1,30 @@
+const createHeader = () => {
+    const headerContainer = document.createElement("div");
+    headerContainer.classList = "header-container";
+  
+    const headerTitle = document.createElement("h1");
+    headerTitle.classList = "header-title";
+    headerTitle.innerText = "Welcome to my restaurant";
+    headerContainer.appendChild(headerTitle);
+  
+    const headerLinksContainer = document.createElement("div");
+    headerLinksContainer.classList = "header-links-container";
+  
+    const headerLinksList = document.createElement("ul");
+    headerLinksList.classList = "header-links-list";
+  
+    const headerLinksArray = ["Home", "Menu", "About"];
+  
+    headerContainer.appendChild(headerLinksContainer);
+    headerLinksContainer.appendChild(headerLinksList);
+  
+    headerLinksArray.forEach((link) => {
+      const li = document.createElement("li");
+      li.innerText = link;
+      headerLinksList.appendChild(li);
+    });
+  
+    return headerContainer;
+  };
+
+  export default createHeader;
