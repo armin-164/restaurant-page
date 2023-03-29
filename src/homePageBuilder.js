3f0fab228daa4212235cbd49d3497277fb8708f2
Armin Sadr <arminsadr164@hotmail.com>
2023-03-29 18:44:54 +0200

    Import and use buildMainSection

--- src/homePageBuilder.js
+++ src/homePageBuilder.js
@@ -1,8 +1,10 @@
 import buildHeader from "./createHeader";
+import buildMainSection from "./createMainSection";
 
 const addElements = () => {
   const content = document.getElementById("content");
   content.appendChild(buildHeader());
+  content.appendChild(buildMainSection());
 };
 
 export default addElements;
