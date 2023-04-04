f8f869277927eee494b6f29b1224686490898b44
Armin Sadr <arminsadr164@hotmail.com>
2023-04-04 13:06:55 +0200

    Work in progress
    Add buildMainSection function to create main section with background image

--- src/createMainSection.js
+++ src/createMainSection.js
@@ -1,6 +1,17 @@
+import pizzaBackground from "./pizza.jpg";
+
+
 const buildMainSection = () => {
     const mainContainer = document.createElement("div");
     mainContainer.classList = "main-container";
+
+    const mainBackgroundImg = document.createElement("img");
+    mainBackgroundImg.src = pizzaBackground;
+    mainContainer.appendChild(mainBackgroundImg);
+
+    const mainContent = document.createElement("div");
+    mainContent.classList = "main-content";
+    mainContainer.appendChild(mainContent);
 
 
     return mainContainer;
