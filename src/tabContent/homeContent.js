87f8e0bb35e5f151bac7e1b0c3de319a4f933013
Armin Sadr <arminsadr164@hotmail.com>
2023-04-06 00:52:14 +0200

    Add homeContent module and export a function
    Add homeContent module to tabContent folder.
    Add a function inside in which the hometab content will be created.

---
+++ src/tabContent/homeContent.js
@@ -0,0 +1,8 @@
+const addHomepageText = () => {
+    const textHeader = document.createElement("h2");
+    textHeader.innerText = "Hello";
+
+    return textHeader;
+}
+
+export default addHomepageText;
