228d84444919c60a50d8c52f8fc896ecf19ac4aa
Armin Sadr <arminsadr164@hotmail.com>
2023-04-06 01:26:09 +0200

    Import function and add it to another functions parameter

--- src/homePageBuilder.js
+++ src/homePageBuilder.js
@@ -1,10 +1,11 @@
 import buildHeader from "./createHeader";
 import buildMainSection from "./createMainSection";
+import addHomepageText from "./tabContent/homeContent";
 
 const addElements = () => {
   const content = document.getElementById("content");
   content.appendChild(buildHeader());
-  content.appendChild(buildMainSection());
+  content.appendChild(buildMainSection(addHomepageText()));
 };
 
 export default addElements;
