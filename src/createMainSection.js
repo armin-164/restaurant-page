5a30339079e2e56526e2965e04da172f769c3b08
Armin Sadr <arminsadr164@hotmail.com>
2023-04-06 01:13:05 +0200

    Add parameter to function and class to an element
    Add a parameter to the buildMainSection function.
    Add a class to the image background element.

--- src/createMainSection.js
+++ src/createMainSection.js
@@ -1,19 +1,21 @@
 import pizzaBackground from "./pizza.jpg";
 
 
-const buildMainSection = () => {
+const buildMainSection = (changeContent) => {
+    
     const mainContainer = document.createElement("div");
     mainContainer.classList = "main-container";
 
     const mainBackgroundImg = document.createElement("img");
     mainBackgroundImg.src = pizzaBackground;
+    mainBackgroundImg.classList = "main-background-img";
     mainContainer.appendChild(mainBackgroundImg);
 
     const mainContent = document.createElement("div");
     mainContent.classList = "main-content";
+    mainContent.appendChild(changeContent);
+
     mainContainer.appendChild(mainContent);
-
-
     return mainContainer;
 }
 
