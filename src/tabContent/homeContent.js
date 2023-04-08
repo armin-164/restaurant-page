8386e4b24092e02765c415a9ef28873b065c94a9
Armin Sadr <arminsadr164@hotmail.com>
2023-04-08 16:18:00 +0200

    Add more content

--- src/tabContent/homeContent.js
+++ src/tabContent/homeContent.js
@@ -1,8 +1,31 @@
 const addHomepageText = () => {
-    const textHeader = document.createElement("h2");
-    textHeader.innerText = "Hello";
+    const containerText = document.createElement("div");
+    containerText.classList = "text-container";
 
-    return textHeader;
+    const promoContainer = document.createElement("div");
+    promoContainer.classList = "promo-container";
+
+    const headerText = document.createElement("h2");
+    headerText.innerText = "Savor the Flavor here at The Italian Place";
+
+    const sloganText = document.createElement("p");
+    sloganText.innerText = "Delicious food at w prices"
+
+    const orderButton = document.createElement("button");
+    orderButton.classList = "order-button";
+    orderButton.innerText = "Order Now";
+
+
+
+    headerText.appendChild(sloganText);
+    promoContainer.appendChild(headerText);
+    promoContainer.appendChild(orderButton);
+
+
+    containerText.appendChild(promoContainer);
+
+    
+    return containerText;
 }
 
 export default addHomepageText;
