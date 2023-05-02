4cc5b11d56ee1e697538ce0fc7b6d72916584c76
Armin Sadr <arminsadr164@hotmail.com>
2023-05-02 23:03:54 +0200

    Add function

--- src/tabContent/homeContent.js
+++ src/tabContent/homeContent.js
@@ -2,6 +2,7 @@ const addHomepageText = () => {
     const containerText = document.createElement("div");
     containerText.classList = "text-container";
 
+    //
     const promoContainer = document.createElement("div");
     promoContainer.classList = "promo-container";
 
@@ -14,8 +15,7 @@ const addHomepageText = () => {
     const orderButton = document.createElement("button");
     orderButton.classList = "order-button";
     orderButton.innerText = "Order Now";
-
-
+    //
 
     headerText.appendChild(sloganText);
     promoContainer.appendChild(headerText);
@@ -23,9 +23,24 @@ const addHomepageText = () => {
 
 
     containerText.appendChild(promoContainer);
+    containerText.appendChild(addInfoContent());
 
     
     return containerText;
+}
+
+function addInfoContent() {
+    const infoContainer = document.createElement("div");
+    infoContainer.classList = "info-container";
+
+    const openingHours = document.createElement("div");
+    openingHours.innerText = "Mon-Thurs:8am-8pm \n Fri-Sun:8am-11pm";
+
+    const addressContainer = document.createElement("div");
+    addressContainer.innerText = "2025 S Washington Ave \n Lansing Charter Township";
+
+    infoContainer.append(openingHours, addressContainer);
+    return infoContainer;
 }
 
 export default addHomepageText;
