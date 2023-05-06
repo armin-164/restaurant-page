9ec820249b9e5666156cb659791b325c8f8a4d83
Armin Sadr <arminsadr164@hotmail.com>
2023-05-06 22:52:13 +0200

    Add two functions
    Add a function that will return price (just for tidyness)
    Add a function that creates menucontent and loops through
    a list of food names and prices and later used to add to DOM

--- src/tabContent/menuContent.js
+++ src/tabContent/menuContent.js
@@ -1,6 +1,29 @@
+function returnPrice(p) {
+    const text = document.createElement("span");
+    text.innerText = `${p} $`
+    return text;
+}
+
+
 function addMenupageText () {
-    let menuText = document.createElement("div");
-    menuText.innerText = "hello";
+    const menuText = document.createElement("div");
+    
+    const foodPriceDiv = document.createElement("div");
+    foodPriceDiv.classList = "food-price-div";
+
+    const foodList = ["Hamburger", "Kebabpizza", "Pasta", "Sushi"];
+    const foodPrices = ["2.99", "8.99", "4.99", "3.99"];
+
+    for (let i = 0; i < foodList.length && i < foodPrices.length; i++) {
+        const food = document.createElement("p");
+        food.innerText = foodList[i];
+
+        food.appendChild(returnPrice(foodPrices[i]));
+
+        foodPriceDiv.appendChild(food);
+    }
+
+    menuText.appendChild(foodPriceDiv);
 
     return menuText;
 }
